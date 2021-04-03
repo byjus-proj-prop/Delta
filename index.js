726834a6ef1a8d2414533529fab530b36dbb4803
idks9 <70767789+idks9@users.noreply.github.com>
2021-04-03 00:01:20 -0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const Delta = {
-    RenderText:function(Type,Value){
-        var text = document.createElement(`${Type}`)
-        text.textContent = `${Value}`
-        document.body.appendChild(text)
-    }
-}
-
-module.exports.Delta = Delta;
